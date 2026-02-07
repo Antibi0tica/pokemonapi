57f72b7c92cc4c6206b4d2cd47d80b6e7da7e20b
Reyjenn <10103560@student.talland.nl>
2026-02-07 16:14:06 +0100

    Added a favourite section in index and placeholder for sprites. Reworked function of fetchData().

--- Js/script.js
+++ Js/script.js
@@ -1,13 +1,34 @@
 function searchresult() {
-    fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Error")
-        } 
 
-        return response.json();
-    })
-    .then(data => console.log(data.name))
-    .catch(error => console.error(error));
+
+    // checks voor Leegstaande input en zorgen dat het een string i
+
+    fetchData();
 }
 
+async function fetchData() {
+    
+    try{
+
+        const pokemonName = document.getElementById("pokemon-search").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+        if (!response.ok){
+            throw new Error("Fout in het ophalen van URL");
+        }
+
+        if (!pokemonName) {
+            throw new Error("Geen naam opgegeven");
+        }
+
+        // if (pokemonName != String()) {
+        //     throw new Error("is een string")
+        // }
+        const data = await response.json();
+        console.log(data.height);
+       
+    } catch(error){
+        console.log(error);
+    }
+        
+    }
