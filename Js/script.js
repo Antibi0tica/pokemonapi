abf9b71d7ac73fa9ad29c5df7dd9d4c415cff46c
Hamsterbalzak1 <rayanamhali12@gmail.com>
2026-02-09 15:45:39 +0100

    Add favorites section and enhance Pokemon display functionality

--- Js/script.js
+++ Js/script.js
@@ -10,25 +10,53 @@ async function fetchData() {
     
     try{
 
+
+        // Setting up const vars for the input and for fetching data from the API website.
         const pokemonName = document.getElementById("pokemon-search").value.toLowerCase();
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
+        
+        var imgSprite = document.getElementById("pokemonSprite");
+
+
         if (!response.ok){
+            hidePokemon(imgSprite)
             throw new Error("Fout in het ophalen van URL");
         }
 
         if (!pokemonName) {
+            hidePokemon(imgSprite)
             throw new Error("Geen naam opgegeven");
         }
 
-        // if (pokemonName != String()) {
-        //     throw new Error("is een string")
-        // }
+
         const data = await response.json();
-        console.log(data.height);
+
+        const sprite = await data.sprites.front_default
+        console.log(data);
+
+         if (await sprite) {
+            imgSprite.scr = sprite;
+            imgSprite.style.display = "block";
+            showPokemon(imgSprite, sprite);
+        }
+
+
+
+
+
+
        
     } catch(error){
         console.log(error);
     }
         
     }
+
+async function savePokemon() {
+
+}
+
+async function hide_show() {
+
+}
