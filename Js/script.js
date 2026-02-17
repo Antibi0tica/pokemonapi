46ecbefc94e3eae6ace9ce971295c350825f67ed
Reyjenn <10103560@student.talland.nl>
2026-02-17 23:10:25 +0100

    Fix favorites section naming and enhance fetchData functionality for better error handling and data display

--- Js/script.js
+++ Js/script.js
@@ -15,18 +15,22 @@ async function fetchData() {
         const pokemonName = document.getElementById("pokemon-search").value.toLowerCase();
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
-        
         var imgSprite = document.getElementById("pokemonSprite");
+        const pokemonDiv = document.getElementById("pokemon-block");
+
+        
 
 
         if (!response.ok){
-            imgSprite.scr = "";
+            pokemonDiv.style.display = "none";
+            imgSprite.src = "";
             imgSprite.style.display = "none";
             throw new Error("Fout in het ophalen van URL");
         }
 
         if (!pokemonName) {
-            imgSprite.scr = "";
+            pokemonDiv.style.display = "none";
+            imgSprite.src = "";
             imgSprite.style.display = "none";
             throw new Error("Geen naam opgegeven");
         }
@@ -34,12 +38,53 @@ async function fetchData() {
 
         const data = await response.json();
 
-        const sprite = await data.sprites.front_default
+        for (const type of data.types) {
+            console.log(type.type.name);
+        }
+
+
+        // stat list
+        const statHp = document.getElementById("pokemon-hp");
+        const statAtk = document.getElementById("pokemon-atk");
+        const statDef = document.getElementById("pokemon-def");
+        const statSAtk = document.getElementById("pokemon-special-atk");
+        const statSDef = document.getElementById("pokemon-special-def");
+        const statSpd = document.getElementById("pokemon-spd");
+
+        // stat data
+
+        
+        for (stats of data.stats) {
+    
+            console.log(stats.stat.name)
+            console.log(stats.base_stat);
+        }
+        const hp = await data.stats[0].base_stat;
+        const atk = await data.stats[1].base_stat;
+        const def = await data.stats[2].base_stat;
+        const specAtk = await data.stats[3].base_stat;
+        const specDef = await data.stats[4].base_stat;
+        const spd = await data.stats[5].base_stat;
+
+        const sprite = await data.sprites.front_default;
         console.log(data);
 
          if (await sprite) {
-            imgSprite.scr = sprite;
+
+            document.getElementById("pokemon-name-holder").innerHTML = data.name;
+
+            // statHp.innerHTML = hp;
+            // statAtk.innerHTML = atk;
+            // statDef.innerHTML = def;
+            // statSAtk.innerHTML = specAtk;
+            // statSDef.innerHTML = specDef;
+            // statSpd.innerHTML = spd;
+
+            pokemonDiv.style.display = "block";
+            imgSprite.src = sprite;
             imgSprite.style.display = "block";
+
+
         }
 
 
@@ -59,5 +104,13 @@ async function savePokemon() {
 }
 
 async function hide_show() {
+
+    
+
+    if (pokemonDiv.style.display === "none") {
+        pokemonDiv.style.display = "block";
+    } else {
+        pokemonDiv.style.display = "none";
+    }
 
 }
